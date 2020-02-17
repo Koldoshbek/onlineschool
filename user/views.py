016ece7de36c062f5616759144475b8b3de4a5a1
Koldoshbek <koldoshbektashiev@gmail.com>
2020-02-17 14:05:23 +0600

    asdas

--- user/views.py
+++ user/views.py
@@ -2,13 +2,11 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
-class ProfileView(TemplateView):
+class UserProfileView(TemplateView):
     template_name = 'user/profile.html'
 
     def get(self, request, *args, **kwargs):
-        context = {}
-        return render(request, self.template_name, context)
+        context = {
 
-    def post(self, request, *args, **kwargs):
-        context = {}
+        }
         return render(request, self.template_name, context)

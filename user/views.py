c0d1c7ac94bb946391f48eee1e2defadee6c3fb2
Koldoshbek <koldoshbektashiev@gmail.com>
2020-02-17 19:25:49 +0600

    user profile done

--- user/views.py
+++ user/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class ProfileView(TemplateView):
+    template_name = 'user/profile.html'
+
+    def get(self, request, *args, **kwargs):
+        context = {}
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        context = {}
+        return render(request, self.template_name, context)
